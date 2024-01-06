a5ce4976e0d8ce0c973a966b2305c8cbabead4e2
ignat <grischuk93@gmail.com>
2024-01-06 21:34:26 +0300

    HomeTask8-5.py

---
+++ HomeTask8-5.py
@@ -0,0 +1,20 @@
+with open('StudentsMarks.txt', 'r') as file:
+    lines = file.readlines()
+
+for line in lines:
+    words = line.split()
+
+    if len(words) == 3:
+        if (words[0].lower() == 'surname' and words[1].lower() ==
+                'name' and words[2].lower() == 'grade'):
+            continue
+        try:
+            surname, name, grade = words
+            grade = int(grade)
+
+            if grade < 3:
+                print(f'{surname} {name}: {grade}')
+        except ValueError:
+            print(f"Ошибка при обработке строки: {line}")
+    else:
+        print(f"Некорректный формат данных в строке: {line}")
