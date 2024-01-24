6cde83ba043bb119dd921421200369dcb8eb45b2
ignat <grischuk93@gmail.com>
2024-01-24 15:28:00 +0300

    Реализация задания с классом Math. Реализация методов для Addition, Subtraction, Multiplication, Division.

--- HomeTask9_№2/HomeTask9_№2.py
+++ HomeTask9_№2/HomeTask9_№2.py
@@ -1,29 +1,42 @@
 class Math:
 
-    def addition(self, value_one, value_two):
-        result = value_one + value_two
-        print(f"Addition: {value_one} + {value_two} = {result}")
+    @staticmethod
+    def addition(value_one, value_two):
+        return value_one + value_two
 
-    def subtraction(self, value_one, value_two):
-        result = value_one + value_two
-        print(f"Subtraction: {value_one} - {value_two} = {result}")
+    @staticmethod
+    def subtraction(value_one, value_two):
+        return value_one - value_two
 
-    def multiplication(self, value_one, value_two):
-        result = value_one * value_two
-        print(f"Multiplication: {value_one} + {value_two} = {result}")
+    @staticmethod
+    def multiplication(value_one, value_two):
+        return value_one * value_two
 
-    def division(self, value_one, value_two):
+    @staticmethod
+    def division(value_one, value_two):
         if value_two != 0:
-            result = value_one / value_two
-            print(f"Division: {value_one} / {value_two} = {result}")
+            return value_one / value_two
         else:
-            print(f"Error: Division by zero is not allowed")
+            print("Error: Division by zero is not allowed")
+            return None
 
 
+# External code
 math_operations = Math()
 
-math_operations.addition(10, 35)
-math_operations.subtraction(55, 70)
-math_operations.multiplication(100, 3000)
-math_operations.division(10, 0)
-math_operations.division(1000, 90)
+result_addition = math_operations.addition(10, 35)
+print(f"Addition: 10 + 35 = {result_addition}")
+
+result_subtraction = math_operations.subtraction(55, 70)
+print(f"Subtraction: 55 - 70 = {result_subtraction}")
+
+result_multiplication = math_operations.multiplication(100, 3000)
+print(f"Multiplication: 100 * 3000 = {result_multiplication}")
+
+result_division = math_operations.division(10, 0)
+if result_division is not None:
+    print(f"Division: 10 / 0 = {result_division}")
+
+result_division_valid = math_operations.division(1000, 90)
+if result_division_valid is not None:
+    print(f"Division: 1000 / 90 = {result_division_valid}")
