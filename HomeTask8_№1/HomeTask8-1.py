ff8323142d318018a6f5399eb8dd43e1c8402431
ignat <grischuk93@gmail.com>
2024-01-25 11:12:40 +0300

    Работа с модулем OS.

--- HomeTask8_№1/HomeTask8-1.py
+++ HomeTask8_№1/HomeTask8-1.py
@@ -38,9 +38,13 @@ def move_files_to_folders(folder_path, sorted_files):
 
 
 def rename_file(file_path, new_name):
-    directory, old_name = os.path.split(file_path)
-    new_path = os.path.join(directory, new_name)
-    os.rename(file_path, new_path)
+    try:
+        directory, old_name = os.path.split(file_path)
+        new_path = os.path.join(directory, new_name)
+        os.rename(file_path, new_path)
+        print(f"Файл {old_name} был переименован в {new_name}")
+    except Exception as e:
+        print(f"Ошибка при переименовании файла {file_path}: {e}")
 
 
 def main():
@@ -54,10 +58,9 @@ def main():
 
     for extension, files in sorted_files.items():
         total_size = 0
-        for extension, files in sorted_files.items():
-            for file in files:
-                file_path = os.path.join(folder_path, extension, file)
-                total_size += os.path.getsize(file_path)
+        for file in files:
+            file_path = os.path.join(folder_path, extension, file)
+            total_size += os.path.getsize(file_path)
 
         print(
             f"В папке с {extension} файлами перемещено {len(files)} файлов, "
@@ -67,7 +70,6 @@ def main():
         file_to_rename = sorted_files['txt'][0]
         new_name = 'some_' + file_to_rename
         rename_file(os.path.join(folder_path, 'txt', file_to_rename), new_name)
-        print(f"Файл {file_to_rename} был переименован в {new_name}")
 
 
 if __name__ == "__main__":
