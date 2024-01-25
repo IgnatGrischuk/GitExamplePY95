f793b1b16904455c6e30274fcfd31a1066fe794c
ignat <grischuk93@gmail.com>
2024-01-25 11:20:25 +0300

    Оценки студентов.

--- HomeTask8_№5/HomeTask8-5.py
+++ HomeTask8_№5/HomeTask8-5.py
@@ -4,17 +4,18 @@ with open('StudentsMarks.txt', 'r') as file:
 for line in lines:
     words = line.split()
 
-    if len(words) == 3:
-        if (words[0].lower() == 'surname' and words[1].lower() ==
-                'name' and words[2].lower() == 'grade'):
-            continue
-        try:
-            surname, name, grade = words
-            grade = int(grade)
+    if (len(words) == 3 and words[0].lower() == 'surname' and words[1].lower()
+            == 'name' and words[2].lower() == 'grade'):
+        continue
 
-            if grade < 3:
-                print(f'{surname} {name}: {grade}')
-        except ValueError:
-            print(f"Ошибка при обработке строки: {line}")
-    else:
+    try:
+        surname, name, grade = words
+        grade = int(grade)
+
+        if grade < 3:
+            print(f'{surname} {name}: {grade}')
+
+    except ValueError:
+        print(f"Ошибка при обработке строки: {line}")
+    except IndexError:
         print(f"Некорректный формат данных в строке: {line}")
