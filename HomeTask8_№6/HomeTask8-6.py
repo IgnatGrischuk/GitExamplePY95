fdd2048212c5d3b9c6d9b9c74a1b5f4862734ae7
ignat <grischuk93@gmail.com>
2024-01-25 11:23:08 +0300

    Сумма всех чисел.

--- HomeTask8_№6/HomeTask8-6.py
+++ HomeTask8_№6/HomeTask8-6.py
@@ -1,17 +1,23 @@
-import re
-
-
-def calculate_summ_of_numbers(filename):
+def calculate_sum_of_numbers(filename):
     total_sum = 0
+    current_number = 0
+
     with open(filename, 'r') as file:
         content = file.read()
-        numbers = re.findall(r'\d+', content)
 
-        for number in numbers:
-            total_sum += int(number)
+        for char in content:
+            if char.isdigit():
+                current_number = current_number * 10 + int(char)
+            else:
+                total_sum += current_number
+                current_number = 0
+
+        # Take into account the last number in the file (if it exists)
+        total_sum += current_number
+
     return total_sum
 
 
 filename = 'Task_6.txt'
-result = calculate_summ_of_numbers(filename)
+result = calculate_sum_of_numbers(filename)
 print(f"Сумма всех чисел в файле: {result}")
